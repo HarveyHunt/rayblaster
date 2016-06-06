6b4f6fb9f1a087a2a54f738175ab62f0b67e5606
Harvey Hunt <harveyhuntnexus@gmail.com>
2016-06-06 23:31:32 +0100

    Start to implement intersect for Sphere

--- src/primitives/sphere.rs
+++ src/primitives/sphere.rs
@@ -1,4 +1,4 @@
-use cgmath::Vector3;
+use cgmath::{Vector3, InnerSpace};
 use primitives::Primitive;
 use renderer::Ray;
 
@@ -21,6 +21,24 @@ impl Sphere {
 
 impl Primitive for Sphere {
     fn intersect(&self, ray: &Ray) -> f64 {
+        let r_squared = self.radius.powi(2);
+        let l = self.pos - ray.origin;
+        let tca = l.dot(ray.direction);
+        if tca < 0.0 {
+            return -1.0;
+        }
+
+        let d2 = l.dot(l) - (tca * tca);
+        if d2 < r_squared {
+            return -1.0;
+        }
+
+        let thc = (r_squared - d2).sqrt();
+
+        let t0 = tca - thc;
+        let t1 = tca + thc;
+
+        // FIXME
         0.0
     }
 
