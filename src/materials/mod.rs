8eff9671d00ac72545cf08b4f9a5412672838e88
Harvey Hunt <harveyhuntnexus@gmail.com>
2017-07-29 17:17:45 +0100

    Add specular material

--- src/materials/mod.rs
+++ src/materials/mod.rs
@@ -1,9 +1,11 @@
 pub mod diffuse;
+pub mod specular;
 
 use cgmath::Vector3;
 
 pub use self::diffuse::DiffuseMaterial;
+pub use self::specular::SpecularMaterial;
 
 pub trait Material {
-    fn sample(&self, hit_normal: Vector3<f64>, dir: Vector3<f64>, l: Vector3<f64>) -> Vector3<f64>;
+    fn sample(&self, hit_normal: Vector3<f64>, ray_dir: Vector3<f64>, l: Vector3<f64>) -> Vector3<f64>;
 }
---
+++ src/materials/specular.rs
@@ -0,0 +1,28 @@
+use materials::Material;
+use cgmath::{Vector3, InnerSpace};
+
+#[derive(Clone)]
+pub struct SpecularMaterial {
+    pub k_diff: f64,
+    pub k_spec: f64,
+    pub shininess: f64,
+    pub diff_colour: Vector3<f64>,
+    pub spec_colour: Vector3<f64>,
+}
+
+impl Material for SpecularMaterial {
+    fn sample(&self, hit_normal: Vector3<f64>, ray_dir: Vector3<f64>, l: Vector3<f64>) -> Vector3<f64> {
+        let half = (l + -ray_dir).normalize();
+
+        let diffuse = 
+        if hit_normal.dot(l) > 0.0 {
+            self.diff_colour * self.k_diff * hit_normal.dot(l)
+        } else {
+            Vector3::new(0.0, 0.0, 0.0)
+        };
+
+        let specular = self.spec_colour * self.k_spec * hit_normal.dot(half).powf(self.shininess);
+
+        diffuse + specular
+    }
+}
