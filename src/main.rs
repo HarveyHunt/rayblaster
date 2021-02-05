9501c2183107c2bef857f87592f770670eb5410a
Harvey Hunt <harveyhuntnexus@gmail.com>
2021-02-05 22:32:55 +0000

    Migrate arg handling to structopt
    Now that clap + structopt exist we can update main to no longer use
    argparse.

--- src/main.rs
+++ src/main.rs
@@ -5,83 +5,70 @@ mod renderer;
 mod scenes;
 
 use anyhow::{anyhow, Context, Error};
-use argparse::{ArgumentParser, Parse, Print, Store};
 use image::save_buffer;
 use renderer::{Renderer, SuperSamplingMode};
+use structopt::StructOpt;
 
 use scenes::scene_lookup;
 use std::path::PathBuf;
 use std::time::Instant;
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "rayblaster")]
+/// A raytracer written in Rust
+struct RayblasterArgs {
+    /// File name to save the rendered scene to
+    #[structopt(short, long, parse(from_os_str))]
+    output_path: PathBuf,
+
+    /// The name of the scene to render
+    #[structopt(short, long)]
+    scene_name: String,
+
+    /// The width of the output image
+    #[structopt(short, long)]
+    width: usize,
+
+    /// The height of the output image
+    #[structopt(short, long)]
+    height: usize,
+
+    /// The FOV of the output image
+    #[structopt(short, long)]
+    fov: f64,
+
+    /// The number of worker threads to spawn
+    #[structopt(short, long)]
+    threads: Option<usize>,
+
+    /// The number of samples per pixel
+    #[structopt(long, default_value = "1")]
+    samples: u16,
+}
+
 fn main() -> Result<(), Error> {
-    let mut image_path = PathBuf::new();
-    let mut scene_name = String::new();
-    let mut width: usize = 0;
-    let mut height: usize = 0;
-    let mut fov: f64 = 0.0;
-    let mut workers = num_cpus::get();
-    let mut samples_arg: u8 = 1;
+    let args = RayblasterArgs::from_args();
 
-    {
-        let mut parser = ArgumentParser::new();
-        parser.set_description("A raytracer written in Rust");
-        parser.add_option(
-            &["-v", "--version"],
-            Print(format!("rayblaster: v{}", env!("CARGO_PKG_VERSION"))),
-            "Show version",
-        );
-        parser
-            .refer(&mut image_path)
-            .add_option(&["-o", "--output"], Parse, "Place the output into <file>")
-            .required();
-        parser
-            .refer(&mut scene_name)
-            .add_option(&["-s", "--scene"], Store, "The scene to render")
-            .required();
-        parser
-            .refer(&mut width)
-            .add_option(&["-w", "--width"], Store, "The width of the output image")
-            .required();
-        parser
-            .refer(&mut height)
-            .add_option(&["-h", "--height"], Store, "The height of the output image")
-            .required();
-        parser
-            .refer(&mut fov)
-            .add_option(&["-f", "--fov"], Store, "The fov of the output image")
-            .required();
-        parser.refer(&mut workers).add_option(
-            &["-t", "--threads"],
-            Store,
-            "The number of worker threads to spawn",
-        );
-        parser.refer(&mut samples_arg).add_option(
-            &["--samples"],
-            Store,
-            "The number of samples per pixel",
-        );
-        parser.parse_args_or_exit();
-    }
+    let mut buffer = vec![0; args.width * args.height * 3].into_boxed_slice();
 
-    let mut buffer = vec![0; width * height * 3].into_boxed_slice();
-
-    let scene =
-        scene_lookup(&scene_name).with_context(|| anyhow!("No primitives in {}", scene_name))?;
+    let scene = scene_lookup(&args.scene_name)
+        .with_context(|| anyhow!("No primitives in {}", args.scene_name))?;
 
     if scene.primitives.is_empty() {
-        return Err(anyhow!("Empty scene: {}", scene_name));
+        return Err(anyhow!("Empty scene: {}", args.scene_name));
     }
 
-    let samples = match samples_arg {
+    let samples = match args.samples {
         1 => SuperSamplingMode::SSAAX1,
         4 => SuperSamplingMode::SSAAX4,
         16 => SuperSamplingMode::SSAAX16,
-        _ => return Err(anyhow!("Unsupported SSAA mode {}", samples_arg)),
+        _ => return Err(anyhow!("Unsupported SSAA mode {}", args.samples)),
     };
 
     let t = Instant::now();
+    let workers = args.threads.unwrap_or_else(num_cpus::get);
 
-    let renderer = Renderer::new(width, height, workers, scene, fov, samples);
+    let renderer = Renderer::new(args.width, args.height, workers, scene, args.fov, samples);
     let frame = renderer.render();
 
     println!(
@@ -95,10 +82,10 @@ fn main() -> Result<(), Error> {
         buffer[i * 3 + 2] = pixel.z;
     }
     save_buffer(
-        image_path,
+        args.output_path,
         &buffer,
-        width as u32,
-        height as u32,
+        args.width as u32,
+        args.height as u32,
         image::RGB(8),
     )
     .map_err(Error::from)
